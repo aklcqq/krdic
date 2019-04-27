c1a8133aeefcf02f997cb99679dc4ea112e54f69
aklcqq <48574784+aklcqq@users.noreply.github.com>
2019-04-27 22:38:03 +0800

    Add files via upload

---
+++ naver_cn_kr.py
@@ -0,0 +1,55 @@
+#!python3
+import urllib.request, urllib.parse, urllib.error
+import ssl
+#import re
+import json
+#import time
+
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from bs4 import BeautifulSoup
+
+#from selenium.common.exceptions import TimeoutException
+#from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+#from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+
+## def
+
+# convert to utf8
+def get_id(vocab):
+    rawurl = 'https://zh.dict.naver.com/api3/zhko/tooltip?query='
+    vocab = urllib.parse.quote(vocab, safe='/',encoding='utf-8')
+    url = rawurl + vocab
+    uhp = urllib.request.Request(url, headers=hdr)
+    uh = urllib.request.urlopen(uhp, context=ctx)
+    data = uh.read().decode()
+#    data = data.rstrip()
+    datajs = json.loads(data.rstrip())
+
+#    jsre = datajs['jsonResult']
+    datajsre = datajs['jsonResult'][0]
+    word_id = datajsre['entryId']
+    return word_id
+
+
+## ignore ssl certificate error
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+hdr = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+
+fn = input('Enter file name: ')
+fh = open(fn)
+word_id_list = list()
+for vocab in fh:
+    try:
+        word_id = get_id(vocab)
+        word_id_list.append(word_id)
+    except:
+        print('NOT FOUND:'+ vocab)
+
+for id in word_id_list:
+    print(id)
+
